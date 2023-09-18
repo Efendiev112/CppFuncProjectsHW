645e222cc8bbf7db0001a5f0e36478c1b33524ec
Vadim <56439970+Efendiev112@users.noreply.github.com>
2023-09-18 15:13:43 +0300

    Задание 1
    программа вычисляет площадь круга, длину окружности и
    диаметр по введенному радиусу

---
+++ dz1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	float p = 3.14;
+	float Rad;
+	cout << "введите радиус круга: ";
+	cin >> Rad;
+
+
+	cout << "Площадь круга =  " << p * (Rad * Rad) << "\n\n";
+	cout << "Длина окружности =  " << 2 * p * Rad << "\n\n";
+	cout << "Диаметр =  " << Rad * 2 << "\n";
+}
