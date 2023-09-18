ae66d140c7902d94bb66ff993c891301bcd08cf2
Vadim <56439970+Efendiev112@users.noreply.github.com>
2023-09-18 15:47:14 +0300

    Задание 3

---
+++ dz3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+
+	float m1;
+	cout << "Введите зарплату за 1 месяц: ";
+	cin >> m1;
+
+
+	float m2;
+	cout << "Введите зарплату за 2 месяц: ";
+	cin >> m2;
+
+
+	float m3;
+	cout << "Введите зарплату за 3 месяц: ";
+	cin >> m3;
+
+	cout << "Итоговая зарплата за квартал: " << m1 + m2 + m3;
+
+
+}
