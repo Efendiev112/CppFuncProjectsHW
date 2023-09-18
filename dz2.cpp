3f89d40ac66b0cff559ccf5a9b1c1e3da7a62a1c
Vadim <56439970+Efendiev112@users.noreply.github.com>
2023-09-18 15:35:55 +0300

    dz2

---
+++ dz2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+
+	float q1;
+	cout << "Оценка за 1 четверть: ";
+	cin >> q1;
+	
+	float q2;
+	cout << "Оценка за 2 четвертьь: ";
+	cin >> q2;
+
+	float q3;
+	cout << "Оценка за 3 четверть: ";
+	cin >> q3;
+
+	float q4;
+	cout << "Оцена за 4 четверть: ";
+	cin >> q4;
+
+	cout << "Итоговая годовая оценка = " << round((q1 + q2 + q3 + q4) / 4);
+
+}
