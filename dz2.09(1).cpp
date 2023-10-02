7e55b431d631bf2ed84b3b30457490e00b7d7d76
Vadim <56439970+Efendiev112@users.noreply.github.com>
2023-10-02 12:36:13 +0300

    dz 2.09(1)

---
+++ dz2.09(1).cpp
@@ -0,0 +1,25 @@
+﻿#include "iostream"
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	char a;
+	cout << "Введите символ ";
+	cin >> a;
+	
+	if (a <= 122 && a >= 97) {
+		cout << a << " - Это английская буква\n";
+	}
+
+	else if (a <= 57 && a >= 48) {
+		cout <<  a << " - Это цифра\n";
+	}
+
+	else if (a == 41 || a == 40 || a == 60 || a == 62 || a == 93 || a == 91 || a == 123 || a == 125) {
+		cout <<  a << "- Это скобка\n";
+	}
+
+	else if (a == 33 || a == 133 || a == 46 || a == 44 || a == 59 || a == 180) {
+		cout <<  a << " - Это знак препинания\n";
+	}
+}
