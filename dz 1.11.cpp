b07a73cdd4032bc5a98de1270430ba8464f512a2
Vadim <56439970+Efendiev112@users.noreply.github.com>
2023-11-07 19:06:49 +0300

    Add files via upload

---
+++ dz 1.11.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    int count = 0;
+    int a, b;
+    cin >> a >> b;
+    bool flag;
+    for (int i = a; i <= b; i++) {
+        flag = true;
+        for (int j = 2; j <= sqrt(i); j++) {
+            if (i % j == 0) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)count++;
+    }
+    cout << count;
+
+}
