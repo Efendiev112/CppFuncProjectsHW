379397c48524c1fd207f88cb3227e4aff8807c0f
Vadim <56439970+Efendiev112@users.noreply.github.com>
2023-11-12 14:39:28 +0300

    Add files via upload

---
+++ DZ15.11(1).cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include<ctime>
+int main()
+{
+    using namespace std;
+    srand(time(NULL));
+    int array[5];
+    int min = 0;
+    int max = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        array[i] = rand();
+        cout << array[i] << " ";
+
+        if (i == 0)
+        {
+            max = array[0];
+            min = array[0];
+        }
+
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+
+
+    }
+    cout << endl << min << " " << max;
+}
